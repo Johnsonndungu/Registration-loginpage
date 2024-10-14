1ad0192e4377743bb3fb1ee996e2ac4251aaee79
Johnson <110740726+Johnsonndungu@users.noreply.github.com>
2024-10-14 20:25:25 +0300

    Login.py

---
+++ Login.py
@@ -0,0 +1,15 @@
+# login.py
+import streamlit as st
+
+st.title("Login Page")
+
+with st.form(key='login_form'):
+    Email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    col1, col2 = st.columns([3, 1])
+    with col1:
+            login_button = st.form_submit_button("Login")
+    with col2:
+            st.write("Don't have an account? [Register](/workspaces/Registration-loginpage/Main.py)") 
+
+# Add your login logic here, e.g., check credentials against a database
